0406928b634bfeb7fcfcacc7276cacc02e9f7e2d
ashishgole30 <ashish.gole3010@gmail.com>
2026-03-12 00:36:45 +0530

    m

--- DAY 04/Basic looping + simulation problems/Subtract_Product_and_Sum_of_Digits.java
+++ DAY 04/Basic looping + simulation problems/Subtract_Product_and_Sum_of_Digits.java
@@ -1,7 +1,12 @@
 public class Subtract_Product_and_Sum_of_Digits {
     public static void main(String[] args) {
         // LEETCODE 1281 – Subtract Product and Sum of Digits
+        /* */
 
+        /* VS CODE Code: */
     }
 
 }
+/* Concepts Used: */
+/* Notes: */
+/* Leetcode Code: */
