f14b20d28cd46ca56d606e69ca417e5996e87b1f
ashishgole30 <ashish.gole3010@gmail.com>
2026-03-17 00:15:08 +0530

    m

--- DAY 06/Prefix Sum with Comparison Logic/Find_Pivot_Index.java
+++ DAY 06/Prefix Sum with Comparison Logic/Find_Pivot_Index.java
@@ -85,4 +85,26 @@ public class Find_Pivot_Index {
  * 11. Return pivotindex
  */
 
-/* Leetcode code: */
+/*
+ * Leetcode code:
+ * class Solution {
+ * public int pivotIndex(int[] nums) {
+ * int n = nums.length;
+ * int[] prefix = new int[n];
+ * prefix[0] = nums[0];
+ * for (int i = 1; i < n; i++) {
+ * prefix[i] = prefix[i - 1] + nums[i];
+ * }
+ * int pivotindex = -1;
+ * for (int i = 0; i < n; i++) {
+ * int leftsum = (i == 0) ? 0 : prefix[i - 1];
+ * int rightsum = prefix[n - 1] - prefix[i];
+ * if (leftsum == rightsum) {
+ * pivotindex = i;
+ * break;
+ * }
+ * }
+ * return pivotindex;
+ * }
+ * }
+ */
